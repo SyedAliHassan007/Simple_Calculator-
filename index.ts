2297d29e8b3b5b577acab64c244113fb0d1b0adc
Your Name <syedalihassan300@gmail.com>
2024-03-22 05:51:48 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt([
+  {
+    message: ((chalk.white.bold.italic)("Enter first number")),
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: ((chalk.white.bold.italic)("Enter Second number")),
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: [ "Addition",
+               "Subtraction",
+               "Multiplication",
+               "Division",],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please Select Valid Operator.");
+}
